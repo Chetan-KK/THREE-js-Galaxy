e0943cc2feb356aabed26ead51e7832fd959ac7b
chetankk <chetankhulage350@gmail.com>
2023-03-27 18:02:07 +0530

    gh pages added

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import glsl from 'vite-plugin-glsl';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [glsl()]
+    base: "/THREE-js-Galaxy/",
+    plugins: [glsl()],
     // server: {
     //     origin: 'http://localhost:3000',
     //     host: '0.0.0.0',
